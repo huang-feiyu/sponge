92c701439ad6242cdb62793317cb2c2f21f7f39f
Ludwig Huang <0711feiyu@gmail.com>
2023-01-03 00:30:44 +0800

    lab3: Debug info

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -2,6 +2,7 @@
 
 #include "tcp_config.hh"
 
+#include <cassert>
 #include <random>
 
 //! \param[in] capacity the capacity of the outgoing bye stream
@@ -11,28 +12,30 @@ TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const s
     : _isn(fixed_isn.value_or(WrappingInt32{std::random_device()()}))
     , _initial_retransmission_timeout{retx_timeout}
     , _retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+    , _stream(capacity) {
+    printf("\n=====init timeout(%d) capacity(%zu)=====\n", retx_timeout, capacity);
+}
 
 uint64_t TCPSender::bytes_in_flight() const { return _bytes_in_flight; }
 
 void TCPSender::fill_window() {
+    TCPSegment segment;
     if (_fin_flag) {
         return;
     }
     if (!_syn_flag) {
+        _syn_flag = true;
         // first hand-shaking: syn & seqno
-        TCPSegment segment;
         segment.header().syn = true;
         send_segment(segment);
         return;
     }
 
     auto window_size = _window_size > 0 ? _window_size : 1;
+    auto remain_size = window_size - (_next_seqno - _recv_seqno);
     // send as much as possible to fill up the window
-    while (!_stream.buffer_empty() && window_size - (_next_seqno - _recv_seqno) > 0) {
-        auto size = std::min(TCPConfig::MAX_PAYLOAD_SIZE, window_size - (_next_seqno - _recv_seqno));
-        TCPSegment segment;
-        segment.payload() = _stream.read(size);
+    while (!_stream.buffer_empty() && remain_size > 0) {
+        segment.payload() = _stream.read(std::min(TCPConfig::MAX_PAYLOAD_SIZE, remain_size));
         // the last segment
         if (_stream.eof()) {
             segment.header().fin = true;
@@ -45,7 +48,10 @@ void TCPSender::fill_window() {
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
 void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
-    uint64_t seqno = unwrap(ackno, _isn, _next_seqno);
+    printf("ack_received: ");
+    auto seqno = unwrap(ackno, _isn, _next_seqno);
+    assert(seqno <= _next_seqno);
+    printf("ack_seqno[%lu] _next_seqno[%lu]\n", seqno, _next_seqno);
     if (seqno <= _recv_seqno) {
         // have been received
         return;
@@ -61,6 +67,11 @@ void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_si
             // everything has been received
             _segments_wait.pop();
             _bytes_in_flight -= segment.length_in_sequence_space();
+            printf("ack_received: received [%lu,%lu) %s\n\tbytes in flight: %lu\n",
+                   unwrap(segment.header().seqno, _isn, _next_seqno),
+                   unwrap(segment.header().seqno, _isn, _next_seqno) + segment.length_in_sequence_space(),
+                   segment.payload().copy().c_str(),
+                   _bytes_in_flight);
         } else {
             break;
         }
@@ -118,4 +129,9 @@ void TCPSender::send_segment(TCPSegment &segment) {
         _retransmission_timer = true;
         _retransmission_timer = 0;
     }
+    printf("send_segment: sent     [%lu,%lu) %s\n\tbytes in flight: %lu\n",
+           unwrap(segment.header().seqno, _isn, _next_seqno),
+           unwrap(segment.header().seqno, _isn, _next_seqno) + segment.length_in_sequence_space(),
+           segment.payload().copy().c_str(),
+           _bytes_in_flight);
 }
