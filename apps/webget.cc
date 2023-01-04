1db198f44d1ecfad671b57347caf59e3e854185b
Ludwig Huang <0711feiyu@gmail.com>
2023-01-04 22:49:46 +0800

    lab4: Revisit webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh"
 #include "util.hh"
 
 #include <cstdlib>
@@ -7,7 +7,7 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    TCPSocket sock{};
+    CS144TCPSocket sock{};
     sock.connect(Address(host, "http"));
     auto request = "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + +"\r\n\r\n";
     sock.write(request);
@@ -15,7 +15,7 @@ void get_URL(const string &host, const string &path) {
     while (!sock.eof()) {
         cout << sock.read();
     }
-    sock.close();
+    sock.wait_until_closed();
 }
 
 int main(int argc, char *argv[]) {
